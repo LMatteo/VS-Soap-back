b85c495aa1da509108854b2ea34361a5ac9180df
Lucas <lucas.matteo@unice.fr>
2018-03-01 15:29:44 +0100

    math ope

---
+++ WcfServiceLibrary1/WcfServiceLibrary1/IMathsOperation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace WcfServiceLibrary1
+{
+    [ServiceContract]
+    public interface IMathsOperation
+    {
+        [OperationContract]
+        int add(int a, int b);
+
+        [OperationContract]
+        int multiply(int a, int b);
+    }
+}
