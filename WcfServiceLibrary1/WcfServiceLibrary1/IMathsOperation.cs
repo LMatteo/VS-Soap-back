63e7d33bdd6da4ff41039b08c387680b06572c7c
Lucas <lucas.matteo@etu.unice.fr>
2018-03-04 14:26:21 +0100

    finishing service

--- WcfServiceLibrary1/WcfServiceLibrary1/IMathsOperation.cs
+++ WcfServiceLibrary1/WcfServiceLibrary1/IMathsOperation.cs
@@ -4,15 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ServiceModel;
+using System.ServiceModel.Web;
 
 namespace WcfServiceLibrary1
 {
     [ServiceContract]
     public interface IMathsOperation
     {
+        [WebInvoke(BodyStyle = WebMessageBodyStyle.WrappedRequest)]
         [OperationContract]
         int add(int a, int b);
 
+        [WebInvoke(BodyStyle = WebMessageBodyStyle.WrappedRequest)]
         [OperationContract]
         int multiply(int a, int b);
     }
